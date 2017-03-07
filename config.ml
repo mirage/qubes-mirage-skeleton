a5211a346ff51552e413a454ef16a53121d40747
Thomas Leonard <talex5@gmail.com>
2017-03-07 20:27:19 +0000

    Get QubesDB and network stack from mirage

--- config.ml
+++ config.ml
@@ -1,16 +1,15 @@
 open Mirage
 
 let main =
-  let packages = [package "mirage-qubes"; package "dns" ~sublibs:["mirage"]; package "tcpip" ~sublibs:["ethif"; "arpv4"; "ipv4"; "icmpv4"; "udp"; "tcp"; "stack-direct"]] in
+  let packages = [
+    package "mirage-qubes";
+    package "dns" ~sublibs:["mirage"];
+  ] in
   foreign
     ~packages
-    "Unikernel.Main" (random @-> time @-> mclock @-> network @-> ethernet @-> arpv4 @-> job)
-
- let net = default_network
-
- let eth = etif net
-
- let arp = arp eth
+    "Unikernel.Main" (qubesdb @-> stackv4 @-> time @-> job)
 
 let () =
-  register "qubes-skeleton" ~argv:no_argv [main $ default_random $ default_time $ default_monotonic_clock $ net $ eth $ arp]
+  register "qubes-skeleton" ~argv:no_argv [
+    main $ default_qubesdb $ qubes_ipv4_stack default_network $ default_time
+  ]
--- unikernel.ml
+++ unikernel.ml
@@ -15,32 +15,13 @@ let rec first_v4 = function
     | None -> first_v4 xs
     | Some ipv4 -> Some ipv4
 
-module Main (Rand: Mirage_random.C)(Time: Mirage_time_lwt.S)(MClock : Mirage_clock.MCLOCK)(Net : Mirage_net_lwt.S)(Eth : Mirage_protocols_lwt.ETHIF with type netif = Net.t)(Arp : Mirage_protocols_lwt.ARP) = struct
-
-  module IP = Static_ipv4.Make(Eth)(Arp)
-  module Icmp = Icmpv4.Make(IP)
-  module Udp = Udp.Make(IP)(Rand)
-  module Tcp = Tcp.Flow.Make(IP)(Time)(MClock)(Rand)
-  module Stack = Tcpip_stack_direct.Make(Time)(Rand)(Net)(Eth)(Arp)(IP)(Icmp)(Udp)(Tcp)
+module Main
+    (DB : Qubes.S.DB)
+    (Stack : Mirage_stack_lwt.V4)
+    (Time : Mirage_time_lwt.S) = struct
 
   (* Initialise DNS resolver *)
   module Resolver = Dns_resolver_mirage.Make(Time)(Stack)
-
-  (* Create IP network device with settings from QubesDB *)
-  let connect db ethif arp =
-    let (>>=?) ip f = match ip with
-      | None -> None
-      | Some s -> f s
-    in
-    let ip = DB.read db "/qubes-ip" >>=? Ipaddr.V4.of_string in
-    let gateway = DB.read db "/qubes-gateway" >>=? Ipaddr.V4.of_string in
-    match ip with
-    | Some ip ->
-      let network = Ipaddr.V4.Prefix.make 32 ip in
-      IP.connect ~ip ~network ~gateway ethif arp
-    | _ -> Lwt.fail_with "couldn't get ip configuration from qubesdb"
-
-  
 
   let get_required qubesDB key =
     match DB.read qubesDB key with
@@ -49,27 +30,20 @@ module Main (Rand: Mirage_random.C)(Time: Mirage_time_lwt.S)(MClock : Mirage_clo
       Log.info (fun f -> f "QubesDB %S = %S" key v);
       v
 
-  let start _random _time clock net ethif arp =
+  let start qubesDB stack _time =
     Log.info (fun f -> f "Starting");
-    (* Start qrexec agent, GUI agent and QubesDB agent in parallel *)
+    (* Start qrexec agent and GUI agent in parallel *)
     let qrexec = RExec.connect ~domid:0 () in
     let gui = GUI.connect ~domid:0 () in
-    let qubesDB = DB.connect ~domid:0 () in
     (* Wait for clients to connect *)
     qrexec >>= fun qrexec ->
     let agent_listener = RExec.listen qrexec Command.handler in
     gui >>= fun gui ->
     Lwt.async (fun () -> GUI.listen gui);
-    qubesDB >>= fun qubesDB ->
     Lwt.async (fun () ->
       OS.Lifecycle.await_shutdown_request () >>= fun (`Poweroff | `Reboot) ->
       RExec.disconnect qrexec
     );
-    connect qubesDB ethif arp >>= fun ip ->
-    Icmp.connect ip >>= fun icmp ->
-    Udp.connect ip >>= fun udp ->
-    Tcp.connect ip clock >>= fun tcp ->
-    Stack.connect Mirage_stack_lwt.({ name = "wibble"; interface = net }) ethif arp ip icmp udp tcp >>= fun stack -> 
     let resolver = Resolver.create stack in
     let dns = get_required qubesDB "/qubes-primary-dns" |> Ipaddr.V4.of_string_exn in
 
